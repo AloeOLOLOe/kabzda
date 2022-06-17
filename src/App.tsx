b7165c4ee115da55cef93c5e0cf2c7ceed98b537
AloeOLOLOe <st.seregin@gmail.com>
2022-06-17 13:31:44 +0300

    5less before hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import './App.css';
+import {Accordion} from "./Accordion";
+import {Rating} from "./Rating";
 
 function App() {
   return (
     <div className="App">
      <Accordion titleValue={'Menu'} collapsed={true}/>
-     <Accordion titleValue={'Users'} collapsed={true}/>
+     <Accordion titleValue={'Users'} collapsed={false}/>
+        <Rating value={1}/>
+        <Rating value={2}/>
+        <Rating value={3}/>
+        <Rating value={4}/>
+        <Rating value={5}/>
     </div>
   );
 }
